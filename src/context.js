c02fadba238025ae6785a3c2b71b19d1aaa7a8ad
Keerthi Mani <carkeane.getz@gmail.com>
2020-02-04 09:43:55 -0500

    axios to fetch api url

--- src/context.js
+++ src/context.js
@@ -1,15 +1,29 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 const Context = React.createContext();
 
 export class Provider extends Component {
   state = {
-    track_list: [
-      { track: { track_name: "ABC" } },
-      { track: { track_name: "123" } }
-    ],
-    heading: "TOP 10 TRACKS"
+    track_list: [],
+    heading: ""
   };
+
+  componentDidMount() {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`
+      )
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          track_list: res.data.message.body.track_list,
+          heading: "Top 10 Tracks"
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
   render() {
     return (
