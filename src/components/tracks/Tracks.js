5a43b7f2765441269405416008e2a7c46abdfebf
Keerthi Mani <carkeane.getz@gmail.com>
2020-02-04 09:49:53 -0500

    import Spinner and destructuring

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 import { Consumer } from "../../context";
+import Spinner from "../layout/Spinner";
 
 class Tracks extends Component {
   render() {
     return (
       <Consumer>
         {value => {
+          const { track_list } = value;
+
           console.log(value);
-          return <h1>Tracks</h1>;
+
+          if (track_list === undefined || track_list === 0) {
+            return <Spinner />;
+          } else {
+            return <h1>Tracks Loaded</h1>;
+          }
         }}
       </Consumer>
     );
